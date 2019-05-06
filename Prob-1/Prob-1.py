61db5c750e40fffda07f95506b61cee6f35890ad
Matt <m.russell6@students.clark.edu>
2019-05-06 15:55:47 -0700

    Prob1Done

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -1,20 +1,23 @@
+#!/usr/bin/env python3
 # Module 3
 #   Programming Assignment 4
 #     Prob-1.py
 
-# <YOUR NAME>
+# <Matt Russell>
 
 def shippingCost(orderSubTotal):
     shippingCost = 2.99
     # enter code here to test for free
-
+    if orderSubTotal >= 10.00:
+        shippingCost = 0.00
 
     return shippingCost
 
-def unitTest():
-    print("Shipping cost if subtotal < 10.00:\t", shippingCost(5.99))
+def unitTest(subtotal):
+    print("Shipping cost if subtotal < 10.00:\t", shippingCost(subtotal))
     # enter additional test code here
+    print("Your subtotal is", subtotal)
 
 
 if __name__ == "__main__":
-    unitTest()
+    unitTest(9.99)
